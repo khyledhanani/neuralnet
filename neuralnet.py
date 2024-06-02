f647c5ae81bb9b2203133e3f7a459b032d1293e2
khyledhanani <khyledhanani@gmail.com>
2024-06-01 21:18:11 -0400

    fixed mistake

--- neuralnet.py
+++ neuralnet.py
@@ -2,75 +2,77 @@ import numpy as np
 
 class NeuralNetwork:
 
-    def __init__(self, inputs: list, outputs: list, hidden_layers: int, hidden_nodes: int, activation_function: callable, activation_derivative: callable):
+    def __init__(self, inputs: np.ndarray, outputs: np.ndarray, hidden_layers: int, hidden_nodes: int, activation_function: callable, activation_derivative: callable):
         self.inputs = inputs
         self.outputs = outputs
-        self.weights = np.random.normal(size=(len(self.inputs[0]), hidden_nodes, hidden_layers))
-        self.biases = np.random.normal(size=(hidden_nodes, hidden_layers))
         self.activation_function = activation_function
         self.activation_derivative = activation_derivative
-
+        
+        # Initialize weights and biases for each layer
+        self.weights = [np.random.normal(size=(inputs.shape[1], hidden_nodes))]
+        self.biases = [np.random.normal(size=(hidden_nodes))]
+        
+        for _ in range(hidden_layers - 1):
+            self.weights.append(np.random.normal(size=(hidden_nodes, hidden_nodes)))
+            self.biases.append(np.random.normal(size=(hidden_nodes)))
+        
+        self.weights.append(np.random.normal(size=(hidden_nodes, outputs.shape[1])))
+        self.biases.append(np.random.normal(size=(outputs.shape[1])))
 
     def feedforward(self, input_data):
         layer_output = input_data
-        self.layer_outputs = [layer_output]  # Initialize as a Python list
+        self.layer_outputs = [layer_output] 
         for weight, bias in zip(self.weights, self.biases):
             layer_output = self.activation_function(np.dot(layer_output, weight) + bias)
             self.layer_outputs.append(layer_output)
         return layer_output
-
     
-    def backprop(self, input_data: np.array, output_data: np.array):
-        gradients_w = []
-        gradients_b = []
-        error = output_data - self.feedforward(input_data)
+    def backprop(self, input_data: np.ndarray, output_data: np.ndarray):
+        gradients_w = [np.zeros_like(w) for w in self.weights]
+        gradients_b = [np.zeros_like(b) for b in self.biases]
+        
+        # Forward pass
+        layer_output = self.feedforward(input_data)
+        error = output_data - layer_output
+        
+        # Backward pass
         for i in reversed(range(len(self.weights))):
             dA_dZ = self.activation_derivative(self.layer_outputs[i + 1])
-            dZ_dW = self.layer_outputs[i]
             delta = error * dA_dZ
-            gradient_w = np.outer(dZ_dW, delta)
-            gradients_w.append(gradient_w)
-            gradients_b.append(delta)
-            error = np.dot(self.weights[i], delta)
+            gradients_w[i] = np.outer(self.layer_outputs[i], delta)
+            gradients_b[i] = delta
+            error = np.dot(delta, self.weights[i].T)
         
-        return gradients_w[::-1], gradients_b[::-1]  # Reverse to match layer order
-
+        return gradients_w, gradients_b
     
     def __call__(self, l_rate, epochs, gd_type):
-        indices = np.arange(len(self.input_data))  
-        total_loss = 0
-
+        indices = np.arange(len(self.inputs))  
         for epoch in range(epochs):
+            total_loss = 0
+
             if gd_type == 'stochastic':
-                np.random.shuffle(indices)  # Shuffle indices for SGD
+                np.random.shuffle(indices)  
             
-            total_gradient_w = 0
-            total_gradient_b = 0
+            total_gradients_w = [np.zeros_like(w) for w in self.weights]
+            total_gradients_b = [np.zeros_like(b) for b in self.biases]
 
             for i in indices:
-                output_layer = self.feedforward(self.input_data[i], self.activation_function)
-                total_loss += np.linalg.norm(self.output_data[i] - output_layer)
-                gradients_w, gradients_b = self.backprop(self.input_data[i], self.output_data[i], self.activation_function)
+                output_layer = self.feedforward(self.inputs[i])
+                total_loss += np.linalg.norm(self.outputs[i] - output_layer)
+                gradients_w, gradients_b = self.backprop(self.inputs[i], self.outputs[i])
 
                 if gd_type == 'batch':
-                    # For batch gradient descent, accumulate gradients over the entire dataset
-                    if total_gradient_w is None:
-                        total_gradient_w = gradients_w
-                        total_gradient_b = gradients_b
-                    else:
-                        total_gradient_w += gradients_w
-                        total_gradient_b += gradients_b
+                    total_gradients_w = [tw + gw for tw, gw in zip(total_gradients_w, gradients_w)]
+                    total_gradients_b = [tb + gb for tb, gb in zip(total_gradients_b, gradients_b)]
                 elif gd_type == 'stochastic':
-                    # For stochastic gradient descent, update weights and biases immediately
-                    self.weights -= l_rate * gradients_w
-                    self.biases -= l_rate * gradients_b
+                    self.weights = [w - l_rate * gw for w, gw in zip(self.weights, gradients_w)]
+                    self.biases = [b - l_rate * gb for b, gb in zip(self.biases, gradients_b)]
 
             if gd_type == 'batch':
-                # Update weights and biases at end of epoch
-                self.weights -= l_rate * total_gradient_w
-                self.biases -= l_rate * total_gradient_b
+                self.weights = [w - l_rate * tw / len(self.inputs) for w, tw in zip(self.weights, total_gradients_w)]
+                self.biases = [b - l_rate * tb / len(self.inputs) for b, tb in zip(self.biases, total_gradients_b)]
 
-            print(f'Epoch {epoch} complete, loss: {total_loss / len(self.input_data)}')
+            print(f'Epoch {epoch} complete, loss: {total_loss / len(self.inputs)}')
 
     """
     Activation Functions:
@@ -92,5 +94,3 @@ class NeuralNetwork:
     @staticmethod
     def relu_derivative(x):
         return np.where(x > 0, 1, 0)
-    
-
